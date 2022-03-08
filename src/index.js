8bf9cabc4036fc5c1843e22f4d63f9470522270d
biggicode <larioncescu@yahoo.com>
2022-03-08 10:58:25 +0200

    create app and index

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { render } from "react-dom"
+import { Provider } from "react-redux"
+import { createStore } from "redux"
+
+import App from "./components/app"
+import reducers from "./reducers"
+
+const app = (
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>
+)
+const root = document.querySelector("#root")
+
+render(app, root)
